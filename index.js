dd52c64ff0de2d66921e684bd6d2a4833669f94e
Sakar Masoud <masoudsakar@gmail.com>
2022-10-24 10:18:42 +0300

    Sakar Masoud

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(array, feel){
+    let array1 = []
+    for (let i = 0 ; i<array.length ; i++){
+        array1.push(`Thank you, ${array[i]}, for the wonderful ${feel} gift!`)
+    }
+    return array1
+}
+
+function countDown(num){
+    for (let i=num ; i>=0 ; i--){
+        console.log(i)
+    }
+}
